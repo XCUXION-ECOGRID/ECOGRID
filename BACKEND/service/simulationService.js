a81a7295ebc7e97530aa3840c9d4c7e6842dac7b
Beast <mjnuvor@gmail.com>
2025-03-28 16:11:43 +0000

    simulationServices implemented

--- BACKEND/service/simulationService.js
+++ BACKEND/service/simulationService.js
@@ -28,3 +28,27 @@ async function calCostCustomePackage(userPackage) {
         breakdown: { panelCost, batteryCost, inverterCost, installationCost: pricing.installationCost }
     }
 }
+
+async function estimateCustomCapacity(appliances, panelPowerRating) {
+
+    let totalDailyConsumption = 0
+
+    appliances.forEach(appliance => {
+        totalDailyConsumption += (appliance.powerRating * appliance.userHours) / 1000
+    });
+
+    const solarPanelEfficiency = 0.2
+    const sunshineHours = 5
+    const DoA = 1
+    const DoD = 0.8
+    const powerFactor = 0.8
+
+    const solarCapacity = ((totalDailyConsumption / solarPanelEfficiency) / sunshineHours).toFixed(2)
+    const panelCount = Math.ceil(solarCapacity / panelPowerRating)
+    const batteryStorage = ((totalDailyConsumption * DoA) / DoD).toFixed(2)
+    const peakPowerUsage = appliances.reduce((total, appliance) => total + appliance.powerRating, 0)
+    const inverterCapacity = (peakPowerUsage / (powerFactor * 1000)).toFixed(2)
+
+    return { totalDailyConsumption, panelCount, batteryStorage, inverterCapacity }
+}
+
