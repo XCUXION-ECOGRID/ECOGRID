6ea35f47bcd32db6959ba59bb73e2f37229d813e
Beast <mjnuvor@gmail.com>
2025-04-08 12:07:53 +0000

    feat: update user schema to include phone, isVerified, and verificationCode fields

--- BACKEND/models/users.js
+++ BACKEND/models/users.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -13,6 +12,20 @@ const userSchema = new mongoose.Schema({
         unique: true,
         lowercase: true,
         trim: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    verificationCode: {
+        type: String,
+        required: true,
     },
     password: {
         type: String,
