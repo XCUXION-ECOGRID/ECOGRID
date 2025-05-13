44cabe096d3933827e0ad4eede64cd2c00af8155
Beast <mjnuvor@gmail.com>
2025-05-13 18:09:46 +0000

    feat: add cart schema for user shopping cart functionality

---
+++ BACKEND/models/homeUser.js
@@ -0,0 +1,9 @@
+// const mongoose = require('mongoose')
+
+// const homeUserSchema = new mongoose.Schema({
+//     cart: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Cart',
+//         required: true
+//     }
+// })
