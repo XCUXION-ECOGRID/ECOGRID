67e8620dae6a93de56a148448cc62fdbc9723bcd
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-17 23:15:17 +0000

    routes for sending forgot password code, validating it and setting a new password

--- BACKEND/routes/userRoute.js
+++ BACKEND/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getAllUsersController, getUserByEmailController, deleteUserByEmailController, updateUserByEmailController } = require('../controllers/userController')
+const { getAllUsersController, getUserByEmailController, deleteUserByEmailController, updateUserByEmailController, forgotPasswordController, verifyForgotPasswordCodeController, setNewPasswordController } = require('../controllers/userController')
 const router = express.Router()
 
 const { authMiddleware, isAdmin } = require('../middleware/auth')
@@ -7,6 +7,10 @@ const { authMiddleware, isAdmin } = require('../middleware/auth')
 router.get('/all', authMiddleware, isAdmin, getAllUsersController)
 router.get('/', authMiddleware, isAdmin, getUserByEmailController)
 router.delete('/delete', authMiddleware, isAdmin, deleteUserByEmailController)
-router.put('/update', authMiddleware, isAdmin, updateUserByEmailController)
+router.put('/update', authMiddleware, isAdmin, updateUserByEmailController);
+router.post('/forgot-password', forgotPasswordController);
+router.post('/verify-code', verifyForgotPasswordCodeController);
+router.post('/newPassword', setNewPasswordController)
+
 
 module.exports = router
