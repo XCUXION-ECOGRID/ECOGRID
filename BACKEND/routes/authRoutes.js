dc1ce6919f09ba5f744c445458ee141fdec70660
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-19 18:13:01 +0000

    specified route for resned verification code

--- BACKEND/routes/authRoutes.js
+++ BACKEND/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { createUserController, verifyCodeController, loginController } = require('../controllers/authController')
+const { createUserController, verifyCodeController, loginController, resendVerificationCodeController } = require('../controllers/authController')
 const router = express.Router()
 
 router.use(express.json())
 
 router.post("/register", createUserController)
+router.post("/resend-code", resendVerificationCodeController)
 router.post("/verify", verifyCodeController)
 router.post("/login", loginController)
 
