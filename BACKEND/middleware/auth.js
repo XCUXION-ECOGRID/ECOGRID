750388885e5faa23ff6708ec1d633f41615f1fe5
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-23 18:13:29 +0000

    no change

--- BACKEND/middleware/auth.js
+++ BACKEND/middleware/auth.js
@@ -9,7 +9,6 @@ function authMiddleware(req, res, next) {
 
     const token = authheader.split(' ')[1]
 
-
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         req.user = decoded
