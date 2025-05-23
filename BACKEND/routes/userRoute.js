28581b683c9f0b6c3e7e1ac1ad2365983f21cf03
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-23 22:02:43 +0000

    modified routes for deleting and updating and included one for getPersonalData

--- BACKEND/routes/userRoute.js
+++ BACKEND/routes/userRoute.js
@@ -1,13 +1,15 @@
 const express = require('express')
-const { getAllUsersController, getUserByEmailController, deleteUserByEmailController, updateUserByEmailController, forgotPasswordController, verifyForgotPasswordCodeController, setNewPasswordController } = require('../controllers/userController')
+const { getAllUsersController, getUserByEmailController, deleteDataController, updateUserByEmailController, forgotPasswordController, verifyForgotPasswordCodeController, setNewPasswordController, getPersonalDataController } = require('../controllers/userController')
 const router = express.Router()
 
-const { authMiddleware, isAdmin } = require('../middleware/auth')
+const { authMiddleware} = require('../middleware/auth')
+const {  isAdmin } = require('../middleware/adminAuth')
 
 router.get('/all', authMiddleware, isAdmin, getAllUsersController)
 router.get('/', authMiddleware, isAdmin, getUserByEmailController)
-router.delete('/delete', authMiddleware, isAdmin, deleteUserByEmailController) // need to update this to make sure that it is accessible to both admin and user
-router.put('/update', authMiddleware, isAdmin, updateUserByEmailController); // should be for both admin and a normal user
+router.get('/me', authMiddleware, getPersonalDataController)
+router.delete('/delete', authMiddleware,  deleteDataController) // need to update this to make sure that it is accessible to both admin and user
+router.put('/update', authMiddleware,  updateUserByEmailController); // should be for both admin and a normal user
 router.post('/forgot-password', forgotPasswordController);
 router.post('/verify-code', verifyForgotPasswordCodeController);
 router.post('/newPassword', setNewPasswordController)
