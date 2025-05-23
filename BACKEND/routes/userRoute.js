7bec7a510baea89e5174dda7df0ac77c114104f8
psam-717 <155116265+psam-717@users.noreply.github.com>
2025-05-23 00:19:20 +0000

    provided comments for next updates

--- BACKEND/routes/userRoute.js
+++ BACKEND/routes/userRoute.js
@@ -6,8 +6,8 @@ const { authMiddleware, isAdmin } = require('../middleware/auth')
 
 router.get('/all', authMiddleware, isAdmin, getAllUsersController)
 router.get('/', authMiddleware, isAdmin, getUserByEmailController)
-router.delete('/delete', authMiddleware, isAdmin, deleteUserByEmailController)
-router.put('/update', authMiddleware, isAdmin, updateUserByEmailController);
+router.delete('/delete', authMiddleware, isAdmin, deleteUserByEmailController) // need to update this to make sure that it is accessible to both admin and user
+router.put('/update', authMiddleware, isAdmin, updateUserByEmailController); // should be for both admin and a normal user
 router.post('/forgot-password', forgotPasswordController);
 router.post('/verify-code', verifyForgotPasswordCodeController);
 router.post('/newPassword', setNewPasswordController)
